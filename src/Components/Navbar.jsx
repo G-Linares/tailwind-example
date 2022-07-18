45bc48d1c3abffc2204d3889d35ddc3e623fa2be
G-Linares <gerrylinares@gmail.com>
2022-07-18 16:13:32 -0500

    finished landing page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import logo from "../Assets/logo.svg";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+  const [burger, setBurger] = useState('');
+  const [menu, setMenu] = useState('hidden')
+
+  const burgerHandler = () => {
+    if(burger==='') {
+      setBurger('open')
+      setMenu('block')
+    } else {
+      setBurger('')
+      setMenu('hidden')
+    }
+  }
+
+
   return (
     <nav className="relative container mx-auto p-6">
       <div className="flex items-center justify-between">
@@ -41,9 +56,24 @@ const Navbar = () => {
             Sign in
           </Link>
         </div>
-        {/* burger menu */}
+        <div className="md:hidden relative z-index-40">
+            <button id="menu-btn" onClick={burgerHandler} className={`z-40 block hamburger md:hidden focus:outline-none ${burger}`}>
+              <span className="hamburger-top"></span>
+              <span className="hamburger-middle"></span>
+              <span className="hamburger-bottom"></span>
+            </button>
+          </div>
       </div>
-      {/* mobile menu */}
+      <div id="menu" className={`absolute p-6 rounded-lg bg-darkViolet left-6 right-6 top-20 z-100 ${menu}`}>
+      <div className="flex flex-col items-center justify-center w-full space-y-6 font-bold text-white rounded-sm">
+
+          <Link to="/" className="w-full text-center">Features</Link>
+          <Link to="/" className="w-full text-center">Pricing</Link>
+          <Link to="/" className="w-full text-center">Resources</Link>
+          <Link to="/" className="w-full pt-6 text-center border-t border-gray-400">Login</Link>
+          <Link to="/" className="w-full py-4 text-center rounded-full bg-cyan">Sign Up</Link>
+      </div>
+        </div>
     </nav>
   );
 };
